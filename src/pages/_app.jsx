86d6d99242949087f2419f109ec0e2d6fc07d436
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2022-04-13 23:38:57 -0500

    initial commit, messing with Next

---
+++ src/pages/_app.jsx
@@ -0,0 +1,11 @@
+
+
+export default function App({ Component, pageProps }) {
+  return (
+ 
+      <div>
+        <Component {...pageProps} />
+      </div>      
+
+  )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default () => (
+  <div>
+    <h1>Index page</h1>
+
+    <Link href="/notes">
+      <a>Notes</a>
+    </Link>
+  </div> 
+)
